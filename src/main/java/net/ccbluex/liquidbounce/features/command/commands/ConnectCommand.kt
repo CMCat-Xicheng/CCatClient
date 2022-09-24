4ffd67db10ba0b26f30310a1dbc065ca68e565b1
CMCat <59408407+CMCat-Xicheng@users.noreply.github.com>
2022-09-24 21:41:35 +0800

    Update ConnectCommand.kt

--- src/main/java/net/ccbluex/liquidbounce/features/command/commands/ConnectCommand.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/command/commands/ConnectCommand.kt
@@ -14,21 +14,21 @@ import net.minecraft.client.gui.GuiMultiplayer
 import net.minecraft.client.multiplayer.GuiConnecting
 import net.minecraft.client.multiplayer.ServerData
 
-class ConnectCommand : Command("connect", emptyArray()) {
+class ConnectCommand : Command("连接", emptyArray()) {
 
     /**
      * Execute commands with provided [args]
      */
     override fun execute(args: Array<String>) {
         if (args.size == 3 && args[2].equals("silent")) {
-            chat("Connecting to §a§l${args[1]} §7(Silent mode)")
+            chat("正在连接到 §f${args[1]} §7(无缝模式)")
             mc.displayGuiScreen(GuiConnecting(GuiMultiplayer(GuiMainMenu()), mc, ServerData("", args[1], false)))
         } else if (args.size == 2) {
-            chat("Connecting to §a§l${args[1]}")
+            chat("正在连接到 §f${args[1]}")
             mc.theWorld.sendQuittingDisconnectingPacket()
             mc.displayGuiScreen(GuiConnecting(GuiMultiplayer(GuiMainMenu()), mc, ServerData("", args[1], false)))
         } else
-            chatSyntax("connect <ip:port> (silent)")
+            chatSyntax("连接 <IP:端口> (无缝)")
     }
 
 }
